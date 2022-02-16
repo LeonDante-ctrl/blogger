7b6a15f89eeb01403adbf94314fae910a566a7c4
Your Name <you@example.com>
2022-02-16 12:12:28 +0300

    added a page to post quote

--- app.py
+++ app.py
@@ -26,9 +26,32 @@ def index():
 def about ():
     return render_template('about.html')
 
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = User.query.filter_by(id=post_id).one()
+    date_posted = 'February 16, 2022'
+    return render_template('post.html', post=post)
+
 @app.route('contact')
 def contact():
     return render_template('contact.html')
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = User(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+    
+    return redirect(url_for('index'))
 if __name__=='__main__':
     app.run(debug=True)
